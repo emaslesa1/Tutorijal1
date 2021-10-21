42ee10d0cf1859f7f6a2df14b16b34f6b6701bc8
emaslesa1 <emaslesa1@etf.unsa.ba>
2021-10-21 12:22:31 +0200

    Initial commit

---
+++ src/ba/unsa/etf/rpr/Lopta.java
@@ -0,0 +1,30 @@
+package ba.unsa.etf.rpr;
+
+public class Lopta {
+    private String naziv;
+    private double poluprecnik;
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+
+    public double getPoluprecnik() {
+        return poluprecnik;
+    }
+
+    public void setPoluprecnik(double poluprecnik) {
+        this.poluprecnik = poluprecnik;
+    }
+    public boolean isIme(){
+        return
+    }
+
+    public Lopta(String naziv, double poluprecnik) {
+        this.naziv = naziv;
+        this.poluprecnik = poluprecnik;
+    }
+}
---
+++ src/ba/unsa/etf/rpr/Main.java
@@ -0,0 +1,10 @@
+package ba.unsa.etf.rpr;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+    String imeVatijable; //prvo malo pa veliko
+    String ImeKlase;
+}
